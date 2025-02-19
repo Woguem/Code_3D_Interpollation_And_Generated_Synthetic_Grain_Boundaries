9ba0d9f72ea7ff560668c477d808fdef5efc6c75
Yen Fred Woguem <woguemyen@gmail.com>
2025-02-19 19:38:23 +0100

    Python codes for interpolate crystal and generating grain boundaries

---
+++ Generated_Synthetic_Cu_001_GB.py
@@ -0,0 +1,385 @@
+"""
+Created on Thu Jan 30 13:07:03 2025
+
+@author: Dr Yen Fred WOGUEM 
+
+@description: This script generates synthetic images of grain boundaries : 
+             grain boundaries with and without noise, noise and vaccum in a material structure.
+"""
+
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import numba as nb
+import math
+from joblib import Parallel, delayed
+import os
+import json
+import scipy.ndimage as ndi
+from datetime import datetime
+
+
+start_time = datetime.now()  # Démarrer le chronomètre
+
+
+
+pixel_size = 0.5
+
+dataset = np.loadtxt('Data_for_genereted_GB_images.txt', skiprows=1) # Download data set containing Nye tensor and the coordinates of atoms
+
+#Positions of atoms
+X = dataset[:, 0]
+Y = dataset[:, 1]
+Z = dataset[:, 2]
+
+#Component of Nye tensor (A)
+A11 = dataset[:,3]
+A12 = dataset[:,4]
+A13 = dataset[:,5]
+
+A21 = dataset[:,6]
+A22 = dataset[:,7]
+A23 = dataset[:,8]
+
+A31 = dataset[:,9]
+A32 = dataset[:,10]
+A33 = dataset[:,11]
+
+
+
+
+# Image generation function with dislocations and/or noise
+
+
+def generate_image(spacing_pixels, num_dislocations, noise_percentage, size_x, size_y, A23, X, Y, noise=True):
+    
+    A23_total = {}
+     
+    
+    key_name = f"{num_dislocations}_{spacing_pixels}"
+    
+    A23_total[key_name] = np.zeros((size_x,size_y))
+
+    for k in range(num_dislocations): 
+        
+        
+        X_pixels = np.round(X / pixel_size).astype(int)  # Angstrom → pixel conversion
+           
+        Y_pixels = np.round(Y / pixel_size).astype(int)  # Angstrom → pixel conversion
+            
+        translated_X = X_pixels + k * spacing_pixels 
+        
+        translated_Y = Y_pixels  # Keep Y unchanged (you can also move it if necessary)
+        
+        # Delete out-of-box values while maintaining X-Y correspondence
+        valid_indices = [i for i in range(len(translated_X)) if translated_X[i] < size_x]
+        translated_X = [translated_X[i] for i in valid_indices]
+        translated_Y = [translated_Y[i] for i in valid_indices]  # Identical filtering
+        A23 = [A23[i] for i in valid_indices]  # Identical filtering
+        
+        
+        shift = 20 # Shift the dislocations to the left so that the space between boundary 1 
+                   # and the 1st dislocation is roughly equal to the space between the last dislocation and boundary 2.
+                    
+        translated_X = [x - shift for x in translated_X]
+            
+        
+        A23_map = np.zeros((size_x,size_y)) # Initialize the matrix map containing the translated dislocation
+        
+        size = len(translated_X)
+        
+        for l in range(size):
+            x_idx = int((translated_X[l]) % size_x)
+            y_idx = int((translated_Y[l]) % size_y)
+            A23_map[y_idx, x_idx]  =  max(A23_map[y_idx, x_idx], A23[l])
+        
+        # Add the map of offset dislocations to the total map
+        
+        A23_total[key_name] += A23_map
+        
+        # Add noise if necessary
+        
+    if noise:
+        num_noise = int(size_x * size_y * noise_percentage)
+        noise_x = np.random.randint(0, size_x, num_noise)
+        noise_y = np.random.randint(0, size_y, num_noise)
+        
+        for i in range(num_noise):
+            A23_total[key_name][noise_y[i], noise_x[i]] = np.random.normal(0, 0.04)
+        
+        
+    return A23_total
+
+
+
+# Generate images with specific proportions
+
+
+size_x =  400   
+size_y =  400   
+
+
+num_images = 3000
+
+spacing_pixels = np.random.randint(10, 200, num_images) # Array containing differents Sapcing between dislocations
+
+num_dislocations = np.array([int(np.round(size_x / n)) for n in spacing_pixels]) # Array containing the number of 
+                                                                                    # dislocations for each spacing 
+
+noise_percentage = 0.05  # 5% of noise
+
+#print(num_dislocations, spacing_pixels, np.shape(spacing_pixels))        
+   
+
+images = {} # Dictionary to collect all images
+
+c1 = 0 # Counter
+c2 = 0 # Counter
+c3 = 0 # Counter
+c4 = 0 # Counter
+
+for n_i in range(1, num_images+1, 1):
+    
+    print(n_i)
+
+    if n_i <= int(0.3*num_images) :  # Create 30% of images with grain boundary dislocations and no other defects
+        c1 += 1
+        A23_total = generate_image(
+            spacing_pixels=spacing_pixels[c1], 
+            num_dislocations=num_dislocations[c1], 
+            noise_percentage=noise_percentage,
+            size_x=size_x, 
+            size_y=size_y, 
+            A23=A23, 
+            X=X, 
+            Y=Y, 
+            noise=False)
+        
+        for key_1 in A23_total.keys():
+            image = A23_total[key_1]
+            key_name_1 = f"{key_1}_GB_system_{c1}"
+            images[key_name_1] = image  # Add image with key
+        
+        print('GB')    
+            
+    elif int(0.3*num_images) < n_i <= int(0.6*num_images) :  # Create 30% of images without defects (vacuum)
+        c2 += 1 
+        A23_total = np.zeros((size_x, size_y))  # Blank image
+        key_name = f"{0}_{0}_vacuum_system_{c2}"
+        images[key_name] = A23_total  # Add image with key
+        
+        print('Vaccum')
+        
+    elif int(0.6*num_images) < n_i <= int(0.8*num_images):  # Create 20% of images with grain boundary dislocations 
+                                                            # and with noise
+        c3 += 1
+        A23_total = generate_image(
+            spacing_pixels=spacing_pixels[c3], 
+            num_dislocations=num_dislocations[c3], 
+            noise_percentage=noise_percentage, 
+            size_x=size_x, 
+            size_y=size_y, 
+            A23=A23, 
+            X=X, 
+            Y=Y, 
+            noise=True)
+        for key_2 in A23_total.keys():
+            image = A23_total[key_2]
+            key_name_2 = f"{key_2}_GB_noise_system_{c3}"
+            images[key_name_2] = image  # Add image with key
+            
+        
+        print('GB_noise')   
+        
+    else:  # Create 20% of images only with noise 
+        c4 += 1
+        A23_total = np.zeros((size_x, size_y))  # Blank image
+        num_noise = int(size_x * size_y * noise_percentage)
+        noise_x = np.random.randint(0, size_x, num_noise)
+        noise_y = np.random.randint(0, size_y, num_noise)
+        for i in range(num_noise):
+            A23_total[noise_y[i], noise_x[i]] = np.random.normal(0, 0.06) # Replacing vacuum with noise
+        key_name = f"{0}_{0}_noise_system_{c4}"
+        images[key_name] = A23_total # Add image with key
+        
+        print('noise')
+    
+        
+
+
+# Define the directory in which to save images and the JSON file
+
+save_dir = r"C:\Users\p09276\Post_doc_Yen_Fred\Projet_Machine_Learning_Julien\GB_Cu_001_Generation\Generated_Images"   
+
+# Create folder if none exists
+os.makedirs(save_dir, exist_ok=True) 
+
+def extract_dislocation_cores(images, threshold=0.4, output_file="Grain_Boundary.json"): #, output_file_1="Max_Dislocations.json"
+    """
+    Detects all pixels with maximum intensity in each image.
+    If the maximum intensity is below the threshold, the image is empty.
+
+    Args:
+        images (dict): Dictionary containing generated images { "key_name": np.array }.
+        threshold (float): Minimum threshold for an image to be considered as containing dislocations.
+        output_file (str): Output JSON file name (full path if necessary).
+
+    Returns:
+            None (writes a JSON file containing annotations).
+    """
+    annotations = {}
+    
+    #max_dislocation = 0
+
+    for i, image in enumerate(images.values()):
+        dislocations = []
+
+        # Find the maximum image intensity
+        max_intensity = np.max(image)
+        
+        #print(max_intensity)
+
+
+        # If the maximum intensity is below the threshold, consider the image as empty 
+                # (because there are vacuum images and images with only noise)
+        if max_intensity < threshold:
+            annotations[f"Image_{i}.png"] = {"dislocations": [{"x": 0, "y": 0, "p1": 0, "p0": 1}]}
+            
+        else :
+
+            # Detection of pixels with intensity greater than 0.4 (first tens of max_intensity)
+            local_maxima =  image >= threshold                                           
+        
+            # Grouping connected maxima into regions
+            labeled_maxima, num_features = ndi.label(local_maxima)
+        
+            #if num_features > max_dislocation : # Sert à récuperer le nombre maximale de dislocations qu'il peut y avoir dans une image
+            
+            #max_dislocation = num_features
+
+            # Find the center of each detected region using the barycenter
+            centers = ndi.center_of_mass(image, labeled_maxima, range(1, num_features + 1))
+            centers = np.array(centers).astype(int)  # Convert to integers
+            # Adding centers detected as dislocations
+            for y, x in centers:
+                dislocations.append({"x": int(x), "y": int(y), "p1": 1, "p0": 0})
+
+            # Save image annotations
+            annotations[f"Image_{i}.png"] = {"dislocations": dislocations}
+
+    
+    # Create a complete path for the output JSON file
+    #output_file_1 = os.path.join(save_dir, 'Max_Dislocations.json')
+    
+    # Save to JSON file in specified location
+    #with open(output_file_1, "w") as f:
+     #   json.dump({"max_dislocations": max_dislocation}, f, indent=4)
+
+    #print(f"Max dislocations saved in {output_file_1}")
+    
+    # Create a complete path for the output JSON file
+    output_file = os.path.join(save_dir, 'Grain_Boundary.json')
+    
+    # Save to JSON file in specified location
+    with open(output_file, "w") as f:
+        json.dump(annotations, f, indent=4)
+
+    print(f"Annotations saved in {output_file}")
+
+
+extract_dislocation_cores(images)  # Label creation
+
+
+
+
+compt_image = -1
+    
+# Display images generated with different configurations
+for key in images.keys():
+    
+    compt_image += 1
+    
+    # Create a figure for each image
+    fig, ax = plt.subplots(figsize=(13.5, 9), dpi=300)
+    
+    # Calculating limits
+    #extent_angstrong = [0, size_x * pixel_size, 0, size_y * pixel_size]
+    
+    extent = [0, size_x, 0, size_y]
+    
+    # Image display with “jet” color map, range of values defined between v_min and v_max
+    img = ax.imshow(images[key], cmap='jet', origin='lower', vmin=-0.3, vmax=0.3, extent=extent)
+    
+    # Add a color bar to the right of the image
+    #cbar = fig.colorbar(img, pad=0.1, location='right')
+    #cbar.set_label(r'$\alpha_{23}$ [$\mathrm{\AA}^{-1}$]', fontsize=40) 
+    #cbar.ax.yaxis.set_label_position('left')
+    #cbar.ax.tick_params(labelsize=34)
+    
+    # Add labels and titles to axes
+    #ax.set_xlabel(r'X [$\mathrm{\AA}$]', fontsize=34)
+    #ax.set_ylabel(r'Y [$\mathrm{\AA}$]', fontsize=34)
+    #ax.tick_params(axis='both', which='major', labelsize=34)
+    
+    ax.set_axis_off() # Disable axis display
+    
+    # Add image title
+    #ax.set_title(f'Number_of_dislocations_and_Spacing_{key}', fontsize=14, fontweight='bold')
+    
+    # Save image as PNG
+    save_path = os.path.join(save_dir, f'Image_{compt_image}.png')
+    
+    plt.savefig(save_path, dpi=300)
+    
+    
+    # Display image in viewer window
+    #plt.show()
+    
+    # Close all figures after display
+    plt.close() 
+
+    
+
+end_time = datetime.now()  # Fin du chronomètre
+execution_time = end_time - start_time
+print(f"\nDurée d'exécution : {execution_time}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Interpolation_for_Synthetic_Cu_001_GB.py
@@ -0,0 +1,411 @@
+"""
+Created on Thu Jan 30 13:07:03 2025
+
+@author: Dr Yen Fred WOGUEM & Dr Houssam KHAROUJI
+
+@description: This script interpolates the crystal structure on a grid using the G-method.
+    
+"""
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
+import time
+import numba as nb
+from scipy.spatial import Voronoi, voronoi_plot_2d
+import itertools
+import math
+from joblib import Parallel, delayed
+from datetime import datetime
+
+
+start_time = datetime.now()  # Start timer
+
+
+
+pixel_size =  0.5
+
+Gij = np.loadtxt('G_Cu_001_365.dump', skiprows=9)
+
+#Positions of atoms
+Xvec = Gij[:, 2]
+Yvec = Gij[:, 3]
+Zvec = Gij[:, 4]
+
+#Component of Nye tensor (A)
+G11 = Gij[:, 5]
+G12 = Gij[:, 6]
+G13 = Gij[:, 7]
+G21 = Gij[:, 8]
+G22 = Gij[:, 9]
+G23 = Gij[:, 10]
+G31 = Gij[:, 11]
+G32 = Gij[:, 12]
+G33 = Gij[:, 13]
+
+
+
+
+#Positions Normalization first time
+Xvec = Xvec - min(Xvec)
+Yvec = Yvec - min(Yvec)
+Zvec = Zvec - min(Zvec)
+
+#System size
+Xsize = max(Xvec)-min(Xvec)
+Ysize = max(Yvec)-min(Yvec)
+Zsize = max(Zvec)-min(Zvec)
+
+
+cutoff = 4
+
+
+# Find the positions of ghost atoms
+
+# Right side ghost atoms
+X_ghost_atoms_x_r = Xvec[np.where(Xvec <= cutoff)] + Xsize
+Y_ghost_atoms_x_r = Yvec[np.where(Xvec <= cutoff)] 
+Z_ghost_atoms_x_r = Zvec[np.where(Xvec <= cutoff)]
+G11_ghost_atoms_x_r = G11[np.where(Xvec <= cutoff)]
+G12_ghost_atoms_x_r = G12[np.where(Xvec <= cutoff)]
+G13_ghost_atoms_x_r = G13[np.where(Xvec <= cutoff)] 
+G21_ghost_atoms_x_r = G21[np.where(Xvec <= cutoff)]
+G22_ghost_atoms_x_r = G22[np.where(Xvec <= cutoff)]
+G23_ghost_atoms_x_r = G23[np.where(Xvec <= cutoff)]
+G31_ghost_atoms_x_r = G31[np.where(Xvec <= cutoff)]
+G32_ghost_atoms_x_r = G32[np.where(Xvec <= cutoff)]
+G33_ghost_atoms_x_r = G33[np.where(Xvec <= cutoff)]
+
+Y_ghost_atoms_y_r = Yvec[np.where(Yvec <= cutoff)] + Ysize
+X_ghost_atoms_y_r = Xvec[np.where(Yvec <= cutoff)] 
+Z_ghost_atoms_y_r = Zvec[np.where(Yvec <= cutoff)] 
+G11_ghost_atoms_y_r = G11[np.where(Yvec <= cutoff)]
+G12_ghost_atoms_y_r = G12[np.where(Yvec <= cutoff)]
+G13_ghost_atoms_y_r = G13[np.where(Yvec <= cutoff)]
+G21_ghost_atoms_y_r = G21[np.where(Yvec <= cutoff)]
+G22_ghost_atoms_y_r = G22[np.where(Yvec <= cutoff)]
+G23_ghost_atoms_y_r = G23[np.where(Yvec <= cutoff)]
+G31_ghost_atoms_y_r = G31[np.where(Yvec <= cutoff)]
+G32_ghost_atoms_y_r = G32[np.where(Yvec <= cutoff)]
+G33_ghost_atoms_y_r = G33[np.where(Yvec <= cutoff)]
+
+Z_ghost_atoms_z_r = Zvec[np.where(Zvec <= cutoff)] + Zsize
+X_ghost_atoms_z_r = Xvec[np.where(Zvec <= cutoff)] 
+Y_ghost_atoms_z_r = Yvec[np.where(Zvec <= cutoff)]
+G11_ghost_atoms_z_r = G11[np.where(Zvec <= cutoff)]
+G12_ghost_atoms_z_r = G12[np.where(Zvec <= cutoff)]
+G13_ghost_atoms_z_r = G13[np.where(Zvec <= cutoff)]
+G21_ghost_atoms_z_r = G21[np.where(Zvec <= cutoff)]
+G22_ghost_atoms_z_r = G22[np.where(Zvec <= cutoff)]
+G23_ghost_atoms_z_r = G23[np.where(Zvec <= cutoff)]
+G31_ghost_atoms_z_r = G31[np.where(Zvec <= cutoff)]
+G32_ghost_atoms_z_r = G32[np.where(Zvec <= cutoff)]
+G33_ghost_atoms_z_r = G33[np.where(Zvec <= cutoff)]
+
+# Left side ghost atoms
+X_ghost_atoms_x_l = Xvec[np.where(Xvec <= cutoff)] - cutoff
+Y_ghost_atoms_x_l = Yvec[np.where(Xvec <= cutoff)] 
+Z_ghost_atoms_x_l = Zvec[np.where(Xvec <= cutoff)] 
+G11_ghost_atoms_x_l = G11[np.where(Xvec <= cutoff)]
+G12_ghost_atoms_x_l = G12[np.where(Xvec <= cutoff)]
+G13_ghost_atoms_x_l = G13[np.where(Xvec <= cutoff)] 
+G21_ghost_atoms_x_l = G21[np.where(Xvec <= cutoff)]
+G22_ghost_atoms_x_l = G22[np.where(Xvec <= cutoff)]
+G23_ghost_atoms_x_l = G23[np.where(Xvec <= cutoff)]
+G31_ghost_atoms_x_l = G31[np.where(Xvec <= cutoff)]
+G32_ghost_atoms_x_l = G32[np.where(Xvec <= cutoff)]
+G33_ghost_atoms_x_l = G33[np.where(Xvec <= cutoff)]
+
+Y_ghost_atoms_y_l = Yvec[np.where(Yvec <= cutoff)] - cutoff
+X_ghost_atoms_y_l = Xvec[np.where(Yvec <= cutoff)] 
+Z_ghost_atoms_y_l = Zvec[np.where(Yvec <= cutoff)] 
+G11_ghost_atoms_y_l = G11[np.where(Yvec <= cutoff)]
+G12_ghost_atoms_y_l = G12[np.where(Yvec <= cutoff)]
+G13_ghost_atoms_y_l = G13[np.where(Yvec <= cutoff)]
+G21_ghost_atoms_y_l = G21[np.where(Yvec <= cutoff)]
+G22_ghost_atoms_y_l = G22[np.where(Yvec <= cutoff)]
+G23_ghost_atoms_y_l = G23[np.where(Yvec <= cutoff)]
+G31_ghost_atoms_y_l = G31[np.where(Yvec <= cutoff)]
+G32_ghost_atoms_y_l = G32[np.where(Yvec <= cutoff)]
+G33_ghost_atoms_y_l = G33[np.where(Yvec <= cutoff)]
+
+Z_ghost_atoms_z_l = Zvec[np.where(Zvec <= cutoff)] - cutoff
+X_ghost_atoms_z_l = Xvec[np.where(Zvec <= cutoff)] 
+Y_ghost_atoms_z_l = Yvec[np.where(Zvec <= cutoff)] 
+G11_ghost_atoms_z_l = G11[np.where(Zvec <= cutoff)]
+G12_ghost_atoms_z_l = G12[np.where(Zvec <= cutoff)]
+G13_ghost_atoms_z_l = G13[np.where(Zvec <= cutoff)]
+G21_ghost_atoms_z_l = G21[np.where(Zvec <= cutoff)]
+G22_ghost_atoms_z_l = G22[np.where(Zvec <= cutoff)]
+G23_ghost_atoms_z_l = G23[np.where(Zvec <= cutoff)]
+G31_ghost_atoms_z_l = G31[np.where(Zvec <= cutoff)]
+G32_ghost_atoms_z_l = G32[np.where(Zvec <= cutoff)]
+G33_ghost_atoms_z_l = G33[np.where(Zvec <= cutoff)]
+
+
+
+# Combine ghost atoms with original vectors
+Xvec = np.concatenate((X_ghost_atoms_x_l, X_ghost_atoms_y_l, X_ghost_atoms_z_l, Xvec, X_ghost_atoms_x_r, X_ghost_atoms_y_r, X_ghost_atoms_z_r))
+Yvec = np.concatenate((Y_ghost_atoms_x_l, Y_ghost_atoms_y_l, Y_ghost_atoms_z_l, Yvec, Y_ghost_atoms_x_r, Y_ghost_atoms_y_r, Y_ghost_atoms_z_r))
+Zvec = np.concatenate((Z_ghost_atoms_x_l, Z_ghost_atoms_y_l, Z_ghost_atoms_z_l, Zvec, Z_ghost_atoms_x_r, Z_ghost_atoms_y_r, Z_ghost_atoms_z_r))
+G11 = np.concatenate((G11_ghost_atoms_x_l, G11_ghost_atoms_y_l, G11_ghost_atoms_z_l, G11, G11_ghost_atoms_x_r, G11_ghost_atoms_y_r, G11_ghost_atoms_z_r))
+G12 = np.concatenate((G12_ghost_atoms_x_l, G12_ghost_atoms_y_l, G12_ghost_atoms_z_l, G12, G12_ghost_atoms_x_r, G12_ghost_atoms_y_r, G12_ghost_atoms_z_r))
+G13 = np.concatenate((G13_ghost_atoms_x_l, G13_ghost_atoms_y_l, G13_ghost_atoms_z_l, G13, G13_ghost_atoms_x_r, G13_ghost_atoms_y_r, G13_ghost_atoms_z_r))
+G21 = np.concatenate((G21_ghost_atoms_x_l, G21_ghost_atoms_y_l, G21_ghost_atoms_z_l, G21, G21_ghost_atoms_x_r, G21_ghost_atoms_y_r, G21_ghost_atoms_z_r))
+G22 = np.concatenate((G22_ghost_atoms_x_l, G22_ghost_atoms_y_l, G22_ghost_atoms_z_l, G22, G22_ghost_atoms_x_r, G22_ghost_atoms_y_r, G22_ghost_atoms_z_r))
+G23 = np.concatenate((G23_ghost_atoms_x_l, G23_ghost_atoms_y_l, G23_ghost_atoms_z_l, G23, G23_ghost_atoms_x_r, G23_ghost_atoms_y_r, G23_ghost_atoms_z_r))
+G31 = np.concatenate((G31_ghost_atoms_x_l, G31_ghost_atoms_y_l, G31_ghost_atoms_z_l, G31, G31_ghost_atoms_x_r, G31_ghost_atoms_y_r, G31_ghost_atoms_z_r))
+G32 = np.concatenate((G32_ghost_atoms_x_l, G32_ghost_atoms_y_l, G32_ghost_atoms_z_l, G32, G32_ghost_atoms_x_r, G32_ghost_atoms_y_r, G32_ghost_atoms_z_r))
+G33 = np.concatenate((G33_ghost_atoms_x_l, G33_ghost_atoms_y_l, G33_ghost_atoms_z_l, G33, G33_ghost_atoms_x_r, G33_ghost_atoms_y_r, G33_ghost_atoms_z_r))
+
+
+
+
+# Positions normalisation second times
+Xvec = Xvec-min(Xvec) 
+Yvec = Yvec-min(Yvec) 
+Zvec = Zvec-min(Zvec)
+
+
+# Calculating minimum and maximum X, Y and Z values
+minX = float(min(Xvec))
+minY = float(min(Yvec))
+minZ = float(min(Zvec))
+
+
+maxX = float(max(Xvec))
+maxY = float(max(Yvec))
+maxZ = float(max(Zvec))
+
+#calculating minimum and maximum X, Y and Z values for ghost atoms
+minX_ghost = float(min(X_ghost_atoms_x_l))
+minY_ghost = float(min(Y_ghost_atoms_y_l))
+minZ_ghost = float(min(Z_ghost_atoms_z_l))
+
+maxX_ghost = float(max(X_ghost_atoms_x_l))
+maxY_ghost = float(max(Y_ghost_atoms_y_l))
+maxZ_ghost = float(max(Z_ghost_atoms_z_l))
+
+
+
+# Calculating number of points in X, Y and Z direction for FFT
+NXvalue = np.ceil((maxX - minX) / pixel_size) + 1
+NYvalue = np.ceil((maxY - minY) / pixel_size) + 1
+NZvalue = np.ceil((maxZ - minZ) / pixel_size) + 1
+
+NX = int(NXvalue)
+NY = int(NYvalue)
+NZ = int(NZvalue)
+
+
+# Calculating number of points of ghost atoms in X, Y and Z direction for FFT
+NXvalue_ghost = np.ceil((maxX_ghost - minX_ghost) / pixel_size) 
+NYvalue_ghost = np.ceil((maxY_ghost - minY_ghost) / pixel_size) 
+NZvalue_ghost = np.ceil((maxZ_ghost - minZ_ghost) / pixel_size) 
+
+
+
+NX_ghost = int(NXvalue_ghost) 
+NY_ghost = int(NYvalue_ghost) 
+NZ_ghost = int(NZvalue_ghost) 
+
+
+# Generating grid points
+x = np.linspace(minX, maxX, NX)
+y = np.linspace(minY, maxY, NY)
+z = np.linspace(minZ, maxZ, NZ)
+
+X, Y, Z = np.meshgrid(x, y, z, indexing='ij')
+
+print (NX, NY, NZ)
+
+
+# Définition des fonctions d'interpolation
+def interpolate_grid(G):
+    print(f"Interpolating {G} on process")  # Affichage pour suivre les tâches
+    return griddata((Xvec, Yvec, Zvec), G, (X, Y, Z), method='linear')
+
+# Liste des matrices à interpoler
+G_components = [G11, G12, G13, G21, G22, G23, G31, G32, G33]
+
+# Exécution parallèle
+results = Parallel(n_jobs=-1)(delayed(interpolate_grid)(G) for G in G_components)
+
+# Stockage des résultats
+G11q, G12q, G13q, G21q, G22q, G23q, G31q, G32q, G33q = results
+
+
+print('End of interpolation')
+
+
+
+@nb.jit
+def calculate_alpha_tensor(G11q, G12q, G13q, G21q, G22q, G23q, G31q, G32q, G33q, NX, NY, NZ, pixel_size):
+    Alpha11 = np.zeros((NX, NY, NZ))
+    Alpha12 = np.zeros((NX, NY, NZ))
+    Alpha13 = np.zeros((NX, NY, NZ))
+    Alpha21 = np.zeros((NX, NY, NZ))
+    Alpha22 = np.zeros((NX, NY, NZ))
+    Alpha23 = np.zeros((NX, NY, NZ))
+    Alpha31 = np.zeros((NX, NY, NZ))
+    Alpha32 = np.zeros((NX, NY, NZ))
+    Alpha33 = np.zeros((NX, NY, NZ))
+
+    for i in range(0, NX - 1, 1):
+        for j in range(0, NY - 1, 1):
+            for k in range(0, NZ - 1, 1):
+                # Handling boundary conditions for i
+                if (i == 0):
+                    im = NX - 2
+                    ip =  1
+                elif (i == NX - 1):
+                    im = NX - 2
+                    ip = 1
+                else:
+                    im = i - 1
+                    ip = i + 1
+                # Handling boundary conditions for j
+                if (j == 0):
+                    jm = NY - 2
+                    jp = 1
+                elif (j == NY - 1):
+                    jm = NY - 2
+                    jp = 1
+                else:
+                    jm = j - 1
+                    jp = j + 1
+                # Handling boundary conditions for k 
+                if (k == 0):  
+                    km = NZ - 2
+                    kp = 1
+                elif (k == NZ - 1):
+                    km = NZ - 2
+                    kp = 1 
+                else:
+                    km = k - 1
+                    kp = k + 1                    
+                 
+
+                Alpha11[i][j][k] = 0.5 * (-(G31q[i][jp][k] - G31q[i][jm][k]) + (G21q[i][j][kp] - G21q[i][j][km])) / pixel_size
+                Alpha12[i][j][k] = 0.5 * ((G31q[ip][j][k] - G31q[im][j][k]) - (G11q[i][j][kp] - G11q[i][j][km])) / pixel_size
+                Alpha13[i][j][k] = 0.5 * (-(G21q[ip][j][k] - G21q[im][j][k]) + (G11q[i][jp][k] - G11q[i][jm][k])) / pixel_size
+
+                Alpha21[i][j][k] = 0.5 * (-(G32q[i][jp][k] - G32q[i][jm][k]) + (G22q[i][j][kp] - G22q[i][j][km])) / pixel_size
+                Alpha22[i][j][k] = 0.5 * ((G32q[ip][j][k] - G32q[im][j][k]) - (G12q[i][j][kp] - G12q[i][j][km])) / pixel_size
+                Alpha23[i][j][k] = 0.5 * (-(G22q[ip][j][k] - G22q[im][j][k]) + (G12q[i][jp][k] - G12q[i][jm][k])) / pixel_size
+
+                Alpha31[i][j][k] = 0.5 * (-(G33q[i][jp][k] - G33q[i][jm][k]) + (G23q[i][j][kp] - G23q[i][j][km])) / pixel_size
+                Alpha32[i][j][k] = 0.5 * ((G33q[ip][j][k] - G33q[im][j][k]) - (G13q[i][j][kp] - G13q[i][j][km])) / pixel_size
+                Alpha33[i][j][k] = 0.5 * (-(G23q[ip][j][k] - G23q[im][j][k]) + (G13q[i][jp][k] - G13q[i][jm][k])) / pixel_size
+
+    return Alpha11, Alpha12, Alpha13, Alpha21, Alpha22, Alpha23, Alpha31, Alpha32, Alpha33
+
+
+# Compute Alpha tensors
+
+A11, A12, A13, A21, A22, A23, A31, A32, A33 = calculate_alpha_tensor(G11q, G12q, G13q, G21q, G22q, G23q, G31q, G32q, G33q, NX, NY, NZ, pixel_size)
+
+
+@nb.jit
+def replace_nan_with_zero(NX, NY, NZ, array):
+    
+    for i in range(NX):
+        for j in range(NY):
+            for k in range(NZ):
+                if np.isnan(array[i, j, k]):
+                    array[i, j, k] = 0
+
+
+# Apply the JIT-compiled function to each array
+replace_nan_with_zero(NX, NY, NZ, A11)
+replace_nan_with_zero(NX, NY, NZ, A12)
+replace_nan_with_zero(NX, NY, NZ, A13)
+replace_nan_with_zero(NX, NY, NZ, A21)
+replace_nan_with_zero(NX, NY, NZ, A22)
+replace_nan_with_zero(NX, NY, NZ, A23)
+replace_nan_with_zero(NX, NY, NZ, A31)
+replace_nan_with_zero(NX, NY, NZ, A32)
+replace_nan_with_zero(NX, NY, NZ, A33)
+
+
+print ('*********************')
+print ('End of interpolation')
+print ('*********************')
+
+
+X = X[int(NX_ghost) : int(NX-NX_ghost) , int(NY_ghost) : int(NY-NY_ghost), int(NZ_ghost) : int(NZ-NZ_ghost)]
+Y = Y[int(NX_ghost) : int(NX-NX_ghost) , int(NY_ghost) : int(NY-NY_ghost), int(NZ_ghost) : int(NZ-NZ_ghost)]
+Z = Z[int(NX_ghost) : int(NX-NX_ghost) , int(NY_ghost) : int(NY-NY_ghost), int(NZ_ghost) : int(NZ-NZ_ghost)]
+
+A11 = A11[int(NX_ghost) : int(NX-NX_ghost) , int(NY_ghost) : int(NY-NY_ghost), int(NZ_ghost) : int(NZ-NZ_ghost)]
+A12 = A12[int(NX_ghost) : int(NX-NX_ghost) , int(NY_ghost) : int(NY-NY_ghost), int(NZ_ghost) : int(NZ-NZ_ghost)]
+A13 = A13[int(NX_ghost) : int(NX-NX_ghost) , int(NY_ghost) : int(NY-NY_ghost), int(NZ_ghost) : int(NZ-NZ_ghost)]
+A21 = A21[int(NX_ghost) : int(NX-NX_ghost) , int(NY_ghost) : int(NY-NY_ghost), int(NZ_ghost) : int(NZ-NZ_ghost)]
+A22 = A22[int(NX_ghost) : int(NX-NX_ghost) , int(NY_ghost) : int(NY-NY_ghost), int(NZ_ghost) : int(NZ-NZ_ghost)] 
+A23 = A23[int(NX_ghost) : int(NX-NX_ghost) , int(NY_ghost) : int(NY-NY_ghost), int(NZ_ghost) : int(NZ-NZ_ghost)] 
+A31 = A31[int(NX_ghost) : int(NX-NX_ghost) , int(NY_ghost) : int(NY-NY_ghost), int(NZ_ghost) : int(NZ-NZ_ghost)] 
+A32 = A32[int(NX_ghost) : int(NX-NX_ghost) , int(NY_ghost) : int(NY-NY_ghost), int(NZ_ghost) : int(NZ-NZ_ghost)]
+A33 = A33[int(NX_ghost) : int(NX-NX_ghost) , int(NY_ghost) : int(NY-NY_ghost), int(NZ_ghost) : int(NZ-NZ_ghost)]
+
+
+
+#Reduice the dimension
+X, Y, Z, A11, A12, A13, A21, A22, A23, A31, A32, A33 = X.flatten(), Y.flatten(), Z.flatten(), A11.flatten(), A12.flatten(), A13.flatten(), A21.flatten(), A22.flatten(), A23.flatten(), A31.flatten(), A32.flatten(), A33.flatten()                
+      
+#Normalize
+X = X - min(X)
+Y = Y - min(Y)
+
+
+#Select the region containing a dislocation and extract the positions and components of the Nye tensor
+
+
+a =  max(X)/2             #Boundaries of mask
+b =  max(X)  
+c = max(Y)/2 
+d = max(Y)
+
+
+#Mask for select the region of one dislocation
+mask = (a <= X) & (X <= b) & (c <= Y) & (Y <= d) 
+
+
+X = X[mask]  
+Y = Y[mask]
+Z = Z[mask]
+
+A11 = A11[mask]
+A12 = A12[mask]
+A13 = A13[mask]
+A21 = A21[mask]
+A22 = A22[mask]
+A23 = A23[mask]
+A31 = A31[mask]
+A32 = A32[mask]
+A33 = A33[mask]
+
+#Normalize
+X = X - min(X)
+Y = Y - min(Y)
+
+
+
+
+# Empiler les tableaux en colonnes
+data = np.column_stack((X, Y, Z, A11, A12, A13, A21, A22, A23, A31, A32, A33))
+
+# Enregistrer dans un fichier texte
+np.savetxt("Data_for_genereted_GB_images.txt", data, fmt="%.10f", delimiter='\t', header='X\tY\tZ\tA11\tA12\tA13\tA21\tA22\tA23\tA31\tA32\tA33', comments='')
+
+print("Fichier data.txt enregistré avec succès.")
+
+
+
+end_time = datetime.now()  # End timer
+execution_time = end_time - start_time
+print(f"\nDurée d'exécution : {execution_time}")
+
+
+
